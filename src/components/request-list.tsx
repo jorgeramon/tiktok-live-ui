72707876bbdbeec31f827a86479ea543dd6505fd
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-15 23:50:27 -0600

    Added scroll to song

--- src/components/request-list.tsx
+++ src/components/request-list.tsx
@@ -6,38 +6,57 @@ import { ListGroup } from 'react-bootstrap';
 export default function () {
   const { state: requests } = useRequests();
 
+  const $requests = requests
+    .filter((request) => !request.completed)
+    .sort(
+      (r1, r2) =>
+        new Date(r1.requested_at).getTime() -
+        new Date(r2.requested_at).getTime()
+    );
+
   useEffect(() => {
     setTimeout(() => {
-      window.scrollTo(0, 0);
+      if ($requests.length > 0) {
+        const element = document.getElementById(
+          $requests[$requests.length - 1]._id
+        );
+
+        window.scrollTo({
+          top: element?.offsetTop,
+        });
+
+        setTimeout(() => {
+          window.scrollTo(0, 0);
+        }, 5000);
+      }
     });
   }, [requests]);
 
   return (
-    <ListGroup variant="flush" className="bg-transparent">
-      {!requests.filter((request) => !request.completed).length ? (
-        <ListGroup.Item
-          as="li"
-          className="fade request-list-item bg-transparent"
-        >
-          No hay canciones pendientes
-        </ListGroup.Item>
-      ) : null}
-      {requests
-        .filter((request) => !request.completed)
-        .sort(
-          (r1, r2) =>
-            new Date(r1.requested_at).getTime() -
-            new Date(r2.requested_at).getTime()
-        )
-        .map(({ _id, request, user_nickname }: IRequest, index: number) => (
+    <>
+      <ListGroup variant="flush" className="bg-transparent">
+        {!$requests.length ? (
           <ListGroup.Item
-            key={_id}
             as="li"
-            className="request-list-item bg-transparent"
+            className="fade request-list-item bg-transparent"
           >
-            {index + 1}. {request} - {user_nickname}
+            No hay canciones pendientes
           </ListGroup.Item>
-        ))}
-    </ListGroup>
+        ) : null}
+        {$requests.map(
+          ({ _id, request, user_nickname }: IRequest, index: number) => (
+            <ListGroup.Item
+              id={_id}
+              key={_id}
+              as="li"
+              className="request-list-item bg-transparent"
+            >
+              {index + 1}. {request} - {user_nickname}
+            </ListGroup.Item>
+          )
+        )}
+      </ListGroup>
+      <div id="empty-space"></div>
+    </>
   );
 }
