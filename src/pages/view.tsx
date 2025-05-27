92f63801efae893d329c85871c6c0d9cefa122e6
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-26 22:10:11 -0600

    Fixed view page

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -3,9 +3,22 @@ import SocketProvider from '@/contexts/socket';
 import { RequestsProvider } from '@/redux/contexts/requests';
 import { Stack } from 'react-bootstrap';
 
+import { RequestsAction } from '@/redux/action';
+import { useRequests } from '@/redux/hooks/requests';
 import '@/view.css';
+import { useEffect } from 'react';
+import { useLoaderData } from 'react-router';
 
 function Page() {
+  const { requests } = useLoaderData();
+  const { dispatch: dispatch_requests } = useRequests();
+
+  useEffect(() => {
+    if (typeof requests !== 'undefined' && requests !== null) {
+      dispatch_requests({ type: RequestsAction.REPLACE_ALL, data: requests });
+    }
+  }, [requests]);
+
   return (
     <Stack>
       <RequestList />
