129b747794f1a7f55f712a0bd6e324e1465b25de
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-26 22:35:13 -0600

    Added user picture preview

--- src/components/request-list.tsx
+++ src/components/request-list.tsx
@@ -1,6 +1,6 @@
 import { IRequest } from '@/interfaces/request';
 import { useRequests } from '@/redux/hooks/requests';
-import { ListGroup } from 'react-bootstrap';
+import { Image, ListGroup, Stack } from 'react-bootstrap';
 
 export default function () {
   const { state: requests } = useRequests();
@@ -22,19 +22,24 @@ export default function () {
           </ListGroup.Item>
         ) : null}
         {$requests.map(
-          ({ _id, request, user_nickname }: IRequest, index: number) => (
+          ({ _id, request, user_nickname, user_picture }: IRequest) => (
             <ListGroup.Item
               id={_id}
               key={_id}
               as="li"
               className="request-list-item bg-transparent"
             >
-              {index + 1}. {request} - {user_nickname}
+              <Stack direction="horizontal" gap={2}>
+                <Image src={user_picture} roundedCircle />
+                <Stack className="ms-3 justify-content-center">
+                  <h3>{user_nickname}</h3>
+                  <h4>{request}</h4>
+                </Stack>
+              </Stack>
             </ListGroup.Item>
           )
         )}
       </ListGroup>
-      <div id="empty-space"></div>
     </>
   );
 }
